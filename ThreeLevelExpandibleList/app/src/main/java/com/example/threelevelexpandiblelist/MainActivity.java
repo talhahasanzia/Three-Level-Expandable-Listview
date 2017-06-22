5aaf81f3cc30c6928dbd9f8a47cb90cdfbad4aae
Talha Hasan Zia <talhahasanzia@hotmail.com>
2017-06-22 21:31:26 +0500

    Dynamic
    Dynamic data added but list generation is not working properly.

--- ThreeLevelExpandibleList/app/src/main/java/com/example/threelevelexpandiblelist/MainActivity.java
+++ ThreeLevelExpandibleList/app/src/main/java/com/example/threelevelexpandiblelist/MainActivity.java
@@ -5,34 +5,73 @@ import android.os.Bundle;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity
-{
-	
-	ExpandableListView expandableListView;
-	
-	String[] parent = new String[]{"Category 1", "Category 2"};
-	
-	
-	String[] category1List = new String[]{"1 one", "1 two", "1 three"};
-	String[] category2List = new String[]{"2 one", "2 two", "2 three", "2 four"};
-	
-	
-	List<String[]> secondLevel = new ArrayList<>();
-	
-	@Override
-	protected void onCreate( Bundle savedInstanceState )
-	{
-		super.onCreate( savedInstanceState );
-		setContentView( R.layout.activity_main );
-		
-		expandableListView=(ExpandableListView)findViewById( R.id.expandible_listview );
-		
-		ParentLevel parentLevelAdapter=new ParentLevel(this, parent,secondLevel);
-		
-		expandableListView.setAdapter(parentLevelAdapter);
-		
-		
-	}
+public class MainActivity extends AppCompatActivity {
+
+    ExpandableListView expandableListView;
+
+    String[] parent = new String[]{"MOVIES", "GAMES"};
+
+
+    String[] movies = new String[]{"Horror", "Action", "Thriller/Drama"};
+    String[] games = new String[]{"FPS", "MOBA", "RPG", "RACING"};
+
+
+    // movies
+    String[] horror = new String[]{"Conjuring", "Insidious", "The Ring"};
+    String[] action = new String[]{"Jon Wick", "Die Hard", "Fast 7", "Avengers"};
+    String[] thriller = new String[]{"Imitation Game", "Tinker, Tailer, Soldier, Spy", "Inception", "Manchester by the Sea"};
+
+
+    // games
+    String[] fps = new String[]{"CS: GO", "Team Fortress 2", "Overwatch", "Battlefield 1", "Halo II", "Warframe"};
+    String[] moba = new String[]{"Dota 2", "League of Legends", "Smite", "Strife", "Heroes of the Storm"};
+    String[] rpg = new String[]{"Witcher III", "Skyrim", "Warcraft", "Mass Effect II", "Diablo", "Dark Souls", "Last of Us"};
+    String[] racing = new String[]{"NFS: Most Wanted", "Forza Motorsport 3", "EA: F1 2016", "Project Cars"};
+
+
+    HashMap<String, String[]> thirdLevelMovies = new HashMap<>();
+    HashMap<String, String[]> thirdLevelGames = new HashMap<>();
+
+
+    List<String[]> secondLevel = new ArrayList<>();
+
+
+    List<HashMap<String, String[]>> data = new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        secondLevel.add(movies);
+        secondLevel.add(games);
+
+        // movies
+        thirdLevelMovies.put(movies[0], horror);
+        thirdLevelMovies.put(movies[1], action);
+        thirdLevelMovies.put(movies[2], thriller);
+
+
+        // games
+        thirdLevelGames.put(games[0], fps);
+        thirdLevelGames.put(games[1], moba);
+        thirdLevelGames.put(games[2], rpg);
+        thirdLevelGames.put(games[2], racing);
+
+
+        data.add(thirdLevelMovies);
+        data.add(thirdLevelGames);
+
+        expandableListView = (ExpandableListView) findViewById(R.id.expandible_listview);
+
+        ParentLevel parentLevelAdapter = new ParentLevel(this, parent, secondLevel,data);
+
+        expandableListView.setAdapter(parentLevelAdapter);
+
+
+    }
 }
--- ThreeLevelExpandibleList/app/src/main/java/com/example/threelevelexpandiblelist/ParentLevel.java
+++ ThreeLevelExpandibleList/app/src/main/java/com/example/threelevelexpandiblelist/ParentLevel.java
@@ -7,91 +7,89 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
 
-public class ParentLevel extends BaseExpandableListAdapter
-{
-	
-	String[] parent;
-	List<String[]> secondLevel;
-	private Context context;
-	
-	public ParentLevel( Context context, String[] parent, List<String[]> secondLevel )
-	{
-		this.context = context;
-		
-		this.parent = parent;
-		
-		this.secondLevel = secondLevel;
-	}
-	
-	@Override
-	public int getGroupCount()
-	{
-		return parent.length;
-	}
-	
-	@Override
-	public int getChildrenCount( int groupPosition )
-	{
-		return secondLevel.get( groupPosition ).length;
-	}
-	
-	@Override
-	public Object getGroup( int groupPosition )
-	{
-		return groupPosition;
-	}
-	
-	@Override
-	public Object getChild( int arg0, int arg1 )
-	{
-		return arg1;
-	}
-	
-	@Override
-	public long getGroupId( int groupPosition )
-	{
-		return groupPosition;
-	}
-	
-	@Override
-	public long getChildId( int groupPosition, int childPosition )
-	{
-		return childPosition;
-	}
-	
-	@Override
-	public boolean hasStableIds()
-	{
-		return true;
-	}
-	
-	@Override
-	public View getGroupView( int groupPosition, boolean isExpanded, View convertView, ViewGroup parent )
-	{
-		if ( convertView == null )
-		{
-			LayoutInflater inflater = (LayoutInflater) context.getSystemService( Context.LAYOUT_INFLATER_SERVICE );
-			convertView = inflater.inflate( R.layout.row_first, null );
-			TextView text = (TextView) convertView.findViewById( R.id.eventsListEventRowText );
-			text.setText( "FIRST LEVEL" );
-		}
-		return convertView;
-	}
-	
-	@Override
-	public View getChildView( int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent )
-	{
-		SecondLevelExpandableListView secondLevelELV = new SecondLevelExpandableListView( context );
-		secondLevelELV.setAdapter( new SecondLevelAdapter( context ) );
-		secondLevelELV.setGroupIndicator( null );
-		return secondLevelELV;
-	}
-	
-	@Override
-	public boolean isChildSelectable( int groupPosition, int childPosition )
-	{
-		return true;
-	}
+public class ParentLevel extends BaseExpandableListAdapter {
+
+    String[] parent;
+    List<String[]> secondLevel;
+    private Context context;
+    List<HashMap<String, String[]>> data;
+
+    public ParentLevel(Context context, String[] parent, List<String[]> secondLevel, List<HashMap<String, String[]>> data) {
+        this.context = context;
+
+        this.parent = parent;
+
+        this.secondLevel = secondLevel;
+
+        this.data = data;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return parent.length;
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+
+        int count = secondLevel.get(groupPosition).length;
+        return 1;
+
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public Object getChild(int group, int child) {
+
+
+        return data.get(group);
+
+
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.row_first, null);
+            TextView text = (TextView) convertView.findViewById(R.id.rowText);
+            text.setText(this.parent[groupPosition]);
+        }
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        SecondLevelExpandableListView secondLevelELV = new SecondLevelExpandableListView(context);
+        secondLevelELV.setAdapter(new SecondLevelAdapter(context, data.get(groupPosition)));
+        secondLevelELV.setGroupIndicator(null);
+        return secondLevelELV;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 }
